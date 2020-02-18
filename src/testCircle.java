7799007582b4331f5089364ba94da99c1a4d46a5
Xuantho01 <hoxuantho95@gmail.com>
2020-02-18 16:42:33 +0700

    circle class

---
+++ src/testCircle.java
@@ -0,0 +1,7 @@
+public class testCircle {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        Circle circle2 = new Circle(10);
+        System.out.println("Area : "+circle2.getArea()+"\nColor: "+circle2.getColor());
+    }
+}
